0d57d497a0dcf212623766d7d0ca1a41e716fc69
Fruzsina Kiss <fruzsinakiss1@gmail.com>
2019-05-15 15:41:12 +0200

    javascript

--- static/js/game.js
+++ static/js/game.js
@@ -3,9 +3,9 @@ function makeBoard(){
     const COL = COLUMN = 10;
     let board = [];
 
-    for (let r = 0; r <= ROW; r++) {
+    for (let r = 0; r < ROW; r++) {
         board[r] = [];
-        for (let c = 0; c <= COL; c++) {
+        for (let c = 0; c < COL; c++) {
             board[r][c] = 0;
         }
     }
@@ -13,3 +13,4 @@ function makeBoard(){
 }
 
 makeBoard();
+console.log(makeBoard());
