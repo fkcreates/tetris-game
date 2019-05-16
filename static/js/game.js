d8a1aae198c04dd3646e9e3222254aefcb97dd4e
gergicsmilan <gergics.milan95@gmail.com>
2019-05-16 10:35:05 +0200

    js code ...

--- static/js/game.js
+++ static/js/game.js
@@ -1,6 +1,17 @@
+const one = [
+    [0,0,0],
+    [1,1,1],
+    [0,1,0],
+];
+
+const playingFigure = {
+    pos: {x:0, y:3},
+    figure: one,
+};
+
 function makeBoard(){
     const ROW = 20;
-    const COL = COLUMN = 10;
+    const COL = 10;
     let board = [];
 
     for (let r = 0; r < ROW; r++) {
@@ -12,5 +23,113 @@ function makeBoard(){
     return board
 }
 
-makeBoard();
-console.log(makeBoard());
+
+function checkGameOver(gameBoard){
+    let counter = 0;
+    let checkedRows = 5;
+    let colNum = 10;
+    for (let col = 0; col < colNum; col++) {
+        for (let row = 0; row < checkedRows; row++) {
+            if (gameBoard[col][row] === 1) {
+                counter += 1;
+                if (counter === checkedRows) {
+                    alert("Game Over!")
+                    return 0;
+                }
+            }
+        }
+        counter = 0;
+    }
+}
+
+
+function getCellByCoordinate(x, y) {
+    return document.querySelector(`.game-cell[data-coordinate-x="${x}"][data-coordinate-y="${y}"]`)
+}
+
+
+function colorChooseForCell(i, j, color){
+    let actualCell = getCellByCoordinate(i, j);
+    actualCell.style.backgroundColor = color;
+}
+
+
+function coloringCells(board){
+    for (let r = 0; r < 20; r++) {
+        for (let c = 0; c < 10; c++) {
+            if (board[c][r] === 1) {
+                colorChooseForCell(r, c, "red");
+            }else if(board[c][r] === 2){
+                colorChooseForCell(r, c, "blue");
+            }
+        }
+    }
+}
+
+
+function addFigureToBoard(board, figure, offset){
+
+    for (let i = 0; i < figure.length; i++) {
+        for (let j = 0; j < figure.length; j++) {
+            if (figure[i][j] !== 0) {
+                board[i+offset.x][j+offset.y] = figure[i][j];
+            }
+        }
+    }
+}
+
+
+function draw(){
+    let gameBoard = makeBoard();
+    addFigureToBoard(gameBoard, playingFigure.figure, playingFigure.pos);
+    coloringCells(gameBoard);
+
+}
+
+
+function main(){
+    let gameBoard = makeBoard();
+    coloringCells(gameBoard);
+
+
+    //checkGameOver(gameBoard);
+    //coloringFigures();
+}
+
+
+let dropStart = Date.now();
+function update(){
+    draw();
+    let now = Date.now();
+    let delta = now - dropStart;
+    console.log(delta);
+    if(delta > 1000){
+        playingFigure.pos.x++;
+        dropStart = Date.now();
+    }
+    requestAnimationFrame(update);
+}
+
+
+
+update();
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*document.addEventListener("keydown", function(event) {
+    if (event.keyCode === 37){
+        //left
+    } else if( event.keyCode === 39){
+        //right
+    }
+});*/
